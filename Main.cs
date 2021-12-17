e46b0bd24ef6ef1c3b0c25991681dd919e276876
WesternGamer <kevnguyen474@gmail.com>
2021-12-16 21:31:23 -0500

    Alpha version, most core features added.

--- Main.cs
+++ Main.cs
@@ -1,45 +1,161 @@
 ﻿using HarmonyLib;
+using RDR2DeathScreen.Patches;
+using Sandbox.Engine.Networking;
+using Sandbox.Engine.Platform;
+using Sandbox.Engine.Utils;
+using Sandbox.Game;
+using Sandbox.Game.Entities;
+using Sandbox.Game.Entities.Character;
+using Sandbox.Game.Gui;
+using Sandbox.Game.GUI;
+using Sandbox.Game.Multiplayer;
+using Sandbox.Game.World;
+using Sandbox.Graphics;
+using System;
+using System.Collections.Generic;
 using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Media;
+using VRage.Audio;
 using VRage.Plugins;
+using VRageMath;
+using VRageRender;
 
-namespace SEPluginTemplate
+namespace RDR2DeathScreen
 {
     public class Main : IPlugin
     {
-        /// <summary>
-        /// Called on startup when plugin loads. Constructor, optional, is not required.
-        /// </summary>
+        ScreenFader ScreenFader;
+
+        MySoundPair DeathSound;
+
+        //IMyAudio myAudio1 = new ();
+
+        public bool IsPlayerAlreadyDead { get; private set; }
+
         public Main()
         {
 
         }
 
-        /// <summary>
-        /// Called when the game closes. This method is required regardless if you are using it or not. If you are not using it, then just don't put anything in the method.
-        /// </summary>
         public void Dispose()
         {
-            
+
         }
 
-        /// <summary>
-        /// Called on startup when plugin gets initialized. This method is required regardless if you are using it or not. If you are not using it, then just don't put anything in the method.
-        /// </summary>
         public void Init(object gameInstance)
         {
-            //Use the code below in this method if you are planning on using Harmony to modify existing game methods. Optional, is not required. For more info about how harmony works, go to https://harmony.pardeike.net/
-            // Starts an instance of Harmony
-            Harmony harmony = new Harmony("SEPluginTemplate");
-            // Patches all patches in the plugin.
+            Harmony harmony = new Harmony("RDR2DeathScreen");
             harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+            ScreenFader = new ScreenFader();
+
+            DeathSound = new MySoundPair("HudDeathAudioSound", true);
         }
 
-        /// <summary>
-        /// Called every game update. This method is required regardless if you are using it or not. If you are not using it, then just don't put anything in the method.
-        /// </summary>
         public void Update()
         {
+            if (MySession.Static != null)
+            {             
+                if (MyVisualScriptLogicProvider.IsPlayerDead(MySession.Static.LocalPlayerId)) //Even though it is the local player Id, it still works on servers, clientside.
+                {
+                    ChangePostProcessSettings();
+                }
+                else
+                {
+                    ChangePostProcessSettingsToDefault();
+                }
+
+                ScreenFader.Update();
+            }
+        }
+
+        private void ChangePostProcessSettings()
+        {
+            MyPostprocessSettingsWrapper.AllEnabled = true;
+            MyPostprocessSettingsWrapper.Settings.Data.Saturation = 0f;
+            MyPostprocessSettingsWrapper.Settings.Data.VignetteLength = 0.5f;
+            MyPostprocessSettingsWrapper.Settings.Data.VignetteStart = 2.5f;
+            //MyPostprocessSettingsWrapper.Settings.Data.LuminanceExposure = 25f;
+            MyPostprocessSettingsWrapper.Settings.Data.BrightnessFactorR = 3.5f;
+            MyPostprocessSettingsWrapper.Settings.Data.BrightnessFactorG = 3.5f;
+            MyPostprocessSettingsWrapper.Settings.Data.BrightnessFactorB = 3.5f;
+            MyPostprocessSettingsWrapper.Settings.Data.GrainStrength = 0.75f;
+            MyPostprocessSettingsWrapper.Settings.Data.GrainAmount = 0.1f;
+
+            MyAudio.Static.Mute = true;
+            MyAudio.Static.StopMusic();
+
+            Rectangle fullscreenRectangle = MyGuiManager.GetFullscreenRectangle();
+            RectangleF rectangleF = new RectangleF(0f, 0f, fullscreenRectangle.Width, fullscreenRectangle.Height);
+            MyRenderProxy.DrawSprite(@"C:\Users\Kevng\Downloads\texturefabrik_dust-03.png", ref rectangleF, null, new VRageMath.Color(255, 255, 255, 10), 0f, true, true, null, null, 0f);
+
             
+            if (ExamplePatch.Instance != null)
+            {
+                if (ExamplePatch.Instance.IsInFirstPersonView == true)
+                    ExamplePatch.Instance.IsInFirstPersonView = false;
+            }
+            
+
+            if (!IsPlayerAlreadyDead)
+            {
+                StartFadeScreen();
+                PlayDeathSound();
+            }
+
+            if (!Sync.MultiplayerActive)
+            {
+                MyFakes.SIMULATION_SPEED = 0.1f;
+            }
+
+
+            IsPlayerAlreadyDead = true;
+        }
+
+        private void ChangePostProcessSettingsToDefault()
+        {
+            MyPostprocessSettingsWrapper.AllEnabled = true;
+            MyPostprocessSettingsWrapper.Settings.Data.Saturation = 1f;
+            MyPostprocessSettingsWrapper.Settings.Data.VignetteLength = 2f;
+            MyPostprocessSettingsWrapper.Settings.Data.VignetteStart = 2f;
+            //MyPostprocessSettingsWrapper.Settings.Data.LuminanceExposure = 1f;
+            MyPostprocessSettingsWrapper.Settings.Data.BrightnessFactorR = 1f;
+            MyPostprocessSettingsWrapper.Settings.Data.BrightnessFactorG = 1f;
+            MyPostprocessSettingsWrapper.Settings.Data.BrightnessFactorB = 1f;
+            MyPostprocessSettingsWrapper.Settings.Data.GrainStrength = 0f;
+            MyPostprocessSettingsWrapper.Settings.Data.GrainAmount = 0.1f;
+
+            MyAudio.Static.Mute = false;
+            MyAudio.Static.PlayMusic();
+
+            ScreenFader.FadeScreen(1f);
+            MyFakes.SIMULATION_SPEED = 1f;
+            IsPlayerAlreadyDead = false;
+        }
+
+        private void StartFadeScreen()
+        {
+            RunTaskWithDelay(4000, delegate
+            {
+                ScreenFader.FadeScreen(0f, 0.65f);
+            });            
+        }
+
+        private async void RunTaskWithDelay(int secondsMs, Action afterDelay)
+        {
+            await Task.Run(async delegate
+            {
+                await Task.Delay(secondsMs);
+                afterDelay();
+            });
+        }
+
+        private IMySourceVoice PlayDeathSound()
+        {
+            return MyAudio.Static.PlaySound(DeathSound.SoundId);
         }
     }
 }
---
+++ ScreenFader.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using Sandbox.Game.Gui;
+using Sandbox.Game.SessionComponents;
+using Sandbox.Game.World;
+using VRageMath;
+using VRageRender;
+
+namespace RDR2DeathScreen
+{
+	//From Sandbox.Game.Gui.MyHudScreenEffects, modified to work with this plugin.
+	internal class ScreenFader
+    {
+		private float m_blackScreenCurrent = 1f;
+
+		private float m_blackScreenStart;
+
+		private float m_blackScreenTimeIncrement;
+
+		private float m_blackScreenTimeTimer;
+
+		private float m_blackScreenTarget = 1f;
+
+		private bool m_blackScreenDataSaved;
+
+		private Color m_blackScreenDataSavedLightColor = Color.Black;
+
+		private Color m_blackScreenDataSavedDarkColor = Color.Black;
+
+		private float m_blackScreenDataSavedStrength;
+
+		public bool BlackScreenMinimalizeHUD = false;
+
+		public Color BlackScreenColor = Color.Black;
+
+		public Action OnBlackscreenFadeFinishedCallback;
+
+		public float BlackScreenCurrent => m_blackScreenCurrent;
+
+		public void Update()
+		{
+			UpdateBlackScreen();
+		}
+
+		public void FadeScreen(float targetAlpha, float time = 0f)
+		{
+			targetAlpha = MathHelper.Clamp(targetAlpha, 0f, 1f);
+			if (time <= 0f)
+			{
+				m_blackScreenTarget = targetAlpha;
+				m_blackScreenCurrent = targetAlpha;
+			}
+			else
+			{
+				m_blackScreenTarget = targetAlpha;
+				m_blackScreenStart = m_blackScreenCurrent;
+				m_blackScreenTimeTimer = 0f;
+				m_blackScreenTimeIncrement = 0.0166666675f / time;
+			}
+			if (targetAlpha < 1f && !m_blackScreenDataSaved)
+			{
+				m_blackScreenDataSaved = true;
+				m_blackScreenDataSavedLightColor = MyPostprocessSettingsWrapper.Settings.Data.LightColor;
+				m_blackScreenDataSavedDarkColor = MyPostprocessSettingsWrapper.Settings.Data.DarkColor;
+				m_blackScreenDataSavedStrength = MyPostprocessSettingsWrapper.Settings.Data.SepiaStrength;
+			}
+		}
+
+		public void SwitchFadeScreen(float time = 0f)
+		{
+			FadeScreen(1f - m_blackScreenTarget, time);
+		}
+
+		public bool IsBlackscreenFadeInProgress()
+		{
+			return m_blackScreenTimeTimer != 1f;
+		}
+
+		private void UpdateBlackScreen()
+		{
+			if (m_blackScreenTimeTimer < 1f && m_blackScreenCurrent != m_blackScreenTarget)
+			{
+				m_blackScreenTimeTimer += m_blackScreenTimeIncrement;
+				if (m_blackScreenTimeTimer > 1f)
+				{
+					m_blackScreenTimeTimer = 1f;
+				}
+				m_blackScreenCurrent = MathHelper.Lerp(m_blackScreenStart, m_blackScreenTarget, m_blackScreenTimeTimer);
+				if (m_blackScreenTimeTimer == 1f && OnBlackscreenFadeFinishedCallback != null)
+				{
+					OnBlackscreenFadeFinishedCallback();
+					OnBlackscreenFadeFinishedCallback = null;
+				}
+			}
+			if (m_blackScreenCurrent < 1f)
+			{
+				if (BlackScreenMinimalizeHUD)
+				{
+					MyHud.CutsceneHud = true;
+				}
+				if (m_blackScreenTarget < m_blackScreenStart)
+				{
+					MyGuiScreenGamePlay.DisableInput = true;
+				}
+				MyPostprocessSettingsWrapper.Settings.Data.LightColor = BlackScreenColor;
+				MyPostprocessSettingsWrapper.Settings.Data.DarkColor = BlackScreenColor;
+				MyPostprocessSettingsWrapper.Settings.Data.SepiaStrength = 1f - m_blackScreenCurrent;
+			}
+			else
+			{
+				MyGuiScreenGamePlay.DisableInput = MySession.Static.GetComponent<MySessionComponentCutscenes>().IsCutsceneRunning;
+				if (m_blackScreenDataSaved)
+				{
+					m_blackScreenDataSaved = false;
+					MyHud.CutsceneHud = MySession.Static.GetComponent<MySessionComponentCutscenes>().IsCutsceneRunning;
+					MyPostprocessSettingsWrapper.Settings.Data.LightColor = m_blackScreenDataSavedLightColor;
+					MyPostprocessSettingsWrapper.Settings.Data.DarkColor = m_blackScreenDataSavedDarkColor;
+					MyPostprocessSettingsWrapper.Settings.Data.SepiaStrength = m_blackScreenDataSavedStrength;
+				}
+			}
+		}
+	}
+}
